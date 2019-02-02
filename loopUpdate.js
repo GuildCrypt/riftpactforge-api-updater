177f90c44f97474aafddbb7cd915f7bae55906eb
Aakil Fernandes <aakilfernandes@gmail.com>
2019-02-02 16:52:24 -0500

    rinkeby

--- loopUpdate.js
+++ loopUpdate.js
@@ -15,45 +15,55 @@ aws.config.update({
 })
 
 const s3 = new aws.S3()
+const targetElapsed = 15000
 
-const mainnetProvider = new Web3HttpProvider(`https://mainnet.infura.io/v3/ddf5fd9bc2314199814e9398df57f486`)
-mainnetProvider.sendAsync = mainnetProvider.send
+startLoopUpdate('mainnet', 'a307b905140c82b37f2d7d806ef9d8858d30ac87')
+setTimeout(() => {
+  startLoopUpdate('rinkeby', '8466730da0d53ceec0d1f564dd462713e676fca6')
+}, targetElapsed / 2)
 
-const oathForgeAddress = Amorph.from(amorphHex.unprefixed, 'a307b905140c82b37f2d7d806ef9d8858d30ac87')
-const oathForgeProviderClient = new OathForgeProviderClient(mainnetProvider, oathForgeAddress)
+function upload(network, oathForgeAddressHexUnprefixed, payload) {
+  return s3.upload({
+    Bucket: 'oathforge-api',
+    ContentType: "application/json",
+    Key: `v0/${network}/${oathForgeAddressHexUnprefixed}`,
+    Body: JSON.stringify(payload, null, 2)
+  }).promise()
+}
 
-function update() {
+function update(network, oathForgeAddressHexUnprefixed, oathForgeProviderClient) {
   return oathForgeProviderClient.fetchOathForgeState().then((oathForgeState) => {
     const payload = {
       retrievedAt: Math.round((new Date).getTime() / 1000),
       data: oathForgeState.toSimplePojo()
     }
-    console.log(JSON.stringify(payload, null, 2))
-    return s3.upload({
-      Bucket: 'oathforge-api',
-      ContentType: "application/json",
-      Key: 'v0/mainnet/a307b905140c82b37f2d7d806ef9d8858d30ac87',
-      Body: JSON.stringify(payload)
-    }).promise()
+    return upload(network, oathForgeAddressHexUnprefixed, payload)
   })
 }
 
-const targetElapsed = 15000
-
-function loopUpdate() {
+function loopUpdate(network, oathForgeAddressHexUnprefixed, oathForgeProviderClient) {
   const startedAt = new Date
-  return update().then(() => {
+  return update(network, oathForgeAddressHexUnprefixed, oathForgeProviderClient).then(() => {
     const finishedAt = new Date
     const elapsed = finishedAt - startedAt
-    console.log(startedAt, finishedAt, elapsed)
+    console.log(network, oathForgeAddressHexUnprefixed, startedAt, finishedAt, elapsed)
     if (elapsed < targetElapsed) {
       return delay(targetElapsed - elapsed)
     } else {
       return delay(0)
     }
   }).then(() => {
-    return loopUpdate()
+    return loopUpdate(network, oathForgeAddressHexUnprefixed, oathForgeProviderClient)
   })
 }
 
-loopUpdate()
+function startLoopUpdate(network, oathForgeAddressHexUnprefixed) {
+  const mainnetProvider = new Web3HttpProvider(`https://${network}.infura.io/v3/ddf5fd9bc2314199814e9398df57f486`)
+  mainnetProvider.sendAsync = mainnetProvider.send
+
+  const oathForgeAddress = Amorph.from(amorphHex.unprefixed, oathForgeAddressHexUnprefixed)
+  const oathForgeProviderClient = new OathForgeProviderClient(mainnetProvider, oathForgeAddress)
+
+  loopUpdate(network, oathForgeAddressHexUnprefixed, oathForgeProviderClient)
+
+}
