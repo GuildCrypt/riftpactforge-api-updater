640a084738333c3f8f8528cd2285ca770e9f6973
Aakil Fernandes <aakilfernandes@gmail.com>
2019-03-16 13:58:30 -0400

    fix riftpactforge rinkeby address

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const UpdateQueue = require('./lib/UpdateQueue')
 const targetElapsed = 15000
 
 //start('mainnet', 'a307b905140c82b37f2d7d806ef9d8858d30ac87')
-start('rinkeby', '4b7fbe965081f1f5626e2662f2bbb352969ed14e')
+start('rinkeby', 'd7b4a7d2bb0ffa29a7d2f17cd6b7e176c48060a6')
 
 function start(network, riftpactforgeAddressHexUnprefixed) {
   const mainnetProvider = new Web3HttpProvider(`https://${network}.infura.io/v3/ddf5fd9bc2314199814e9398df57f486`)
